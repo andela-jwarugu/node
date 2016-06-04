7736269a308d3dbddd444de9c17f8959a6164646
andela-jwarugu <joy.warugu@andela.com>
2016-06-04 15:14:36 +0300

    Add http request file

---
+++ http.js
@@ -0,0 +1,17 @@
+var http = require('http');
+
+http.get(process.argv[2], function (response){
+	//console.log('Got response:' + response.statusCode);
+	response.on('data', function(data){
+		console.log(data.toString());	
+	});
+})
+
+//Ideal Answer
+// var http = require('http')
+
+// http.get(process.argv[2], function (response){
+// 	response.setEncoding('utf8');
+// 	response.on('data', console.log);
+// 	response.on('error', console.error);
+// }).on('error', console.error)
